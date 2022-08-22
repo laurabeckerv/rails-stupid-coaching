696e9cf7760ee95a6bb84c6cff08a443b478f848
Laura Beckervordersandforth <laura.beckerv@gmail.com>
2022-08-22 15:00:24 +0200

    structure finalized

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,18 @@
 class QuestionsController < ApplicationController
 
+  # routes back ask from the view
   def ask
+  end
+
+  # routes back answer from the view
+  def answer
+    @question = params[:question]
+    if @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I dont care, get dressed and go to work!"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # get "ask", to: "controller#action"
-  get "ask", to: "questions#ask"
+  get "ask", to: "questions#ask", as: :ask
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "answer", to: "questions#answer", as: :answer
 end
